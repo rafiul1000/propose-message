bf1bd744469f4225b4a5a912cbacef9545912eb8
rafiul1000 <rafiulislam5075@gmail.com>
2023-08-09 17:17:27 +0600

    Propose message using js

---
+++ propose-using-js/index.js
@@ -0,0 +1,7 @@
+document.getElementById('yes-btn').addEventListener("click", function(){
+    document.getElementById('msg').innerText="I LOVE YOU 💘";
+})
+
+function proposeSender(){
+    document.getElementById('msg').innerText="I LOVE YOU 💘";
+}
